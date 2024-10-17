2d850bbc392676c1e4b648158fce80f397683521
Tetrapod108 <kzking.hrn@icloud.com>
2024-10-17 23:55:01 +0900

    箱がぶるぶる震えるモーション作成

---
+++ main.py
@@ -0,0 +1,82 @@
+import sys
+import pygame
+from pygame.locals import QUIT, MOUSEBUTTONDOWN
+
+pygame.init()
+SURFACE_X_SIZE = 1920
+SURFACE_Y_SIZE = 1080
+SURFACE = pygame.display.set_mode((SURFACE_X_SIZE, SURFACE_Y_SIZE))
+FPSCLOCK = pygame.time.Clock()
+pygame.display.set_caption("LootChestSystem")
+
+# クラス: チェスト
+class Chest:
+    # コンストラクタ
+    def __init__(self, pos, path):
+        self.image = pygame.image.load(path)
+        self.pos = pos
+        self.theta = 0
+    
+    # status = "standby"時に実行
+    def standby(self):
+        global process
+        global status
+        max_process = 100
+        if 0 <= process % max_process < 8:
+            self.theta -= 5
+        if 8 <= process % max_process < 24:
+            self.theta += 5
+        if 24 <= process % max_process < 40:
+            self.theta -= 5
+        if 40 <= process % max_process < 48:
+            self.theta += 5
+        if process % max_process == max_process - 1:
+            status = "empty"
+
+        self.new_image = pygame.transform.rotate(self.image, self.theta)
+        self.rect = self.new_image.get_rect()
+        self.rect.center = (self.pos[0], self.pos[1])
+
+    # 毎tick実行
+    def tick(self):
+        global status
+        if status == "standby":
+            self.standby()
+
+    # 描画を更新
+    def draw(self):
+        SURFACE.blit(self.new_image, self.rect)
+
+status = "standby"
+process = 0
+def main():
+    global status
+    global process
+    next_status = " "
+    chest = Chest((SURFACE_X_SIZE / 2, SURFACE_Y_SIZE / 2), "chest.png")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                if event.dict["button"] == 3:
+                    pygame.quit()
+                    sys.exit()
+                next_status = "open"
+        
+        chest.tick()
+
+        SURFACE.fill((255, 255, 255))
+        chest.draw()
+
+        if status == "empty" and next_status == " ":
+            status = "standby"
+
+        process += 1
+        pygame.display.update()
+        FPSCLOCK.tick(60)
+
+if __name__ == "__main__":
+    main()
