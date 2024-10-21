6c622c7fa26805d21be0bf9b6e1a4fec7f8c742d
Tetrapod108 <kzking.hrn@icloud.com>
2024-10-21 12:45:20 +0900

    lotteryの追記

--- lottery.py
+++ lottery.py
@@ -1,4 +1,15 @@
 import random
 
+# database[0]がレア度:通常、database[1]がレア度:シークレット
+database = [[1, 2, 3, 4, 5, 6],
+            [11, 12, 13, 14, 15]]
+
 def lottery():
-    print()
+    rnd = random.randint(1, 100)
+    if 0 <= rnd < 90:
+        result = random.choice(database[0])
+    if 90 <= rnd < 100:
+        result = random.choice(database[1])
+    return result
+
+print(lottery())
